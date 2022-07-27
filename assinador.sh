7c05d2cb52fb8ec6f354525420f76782b7151ab2
Cledson Ferreira <cledsonitgames@gmail.com>
2022-07-27 12:02:40 -0300

    Remoção de código licenciado e outras alterações

--- assinador.sh
+++ assinador.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
 # O argumento da linha de comando, é o endereço da pasta onde as faturas serão diretamente exportadas
-[ -z "$1" ] && exit 1
-cd "$1"
-[ $? -ne 0 ] && exit 2
+if [ -n "$1" ]; then
+  cd "$1"
+  [ $? -ne 0 ] && exit 2
+else
+  cd "faturas/"
+  [ $? -ne 0 ] && exit 2
+fi
 
+echo " ---- INICIADO ÀS $(date +'%H:%M de %d/%m/%Y') ---- "
 # Caso o diretório exista, começamos um loop de procura por arquivos a serem assinados
 while [ 0 ]; do
-  arq="$(ls | grep -Eom 1 'Fatura - [- [:digit:]]+\.pdf')"
-  if [ -n "$arq" ] && [ -r "$arq" ]; then
-    echo "Encontrado: $arq"
+  if [ -r "assinar.pdf" ]; then
+    arq="assinar.pdf"
 
-    data="$(echo $arq | sed -E 's/.*- ([-[:digit:]]+).*/\1/g')"
-    hora=$(echo $arq | sed -E 's/.* ([[:digit:]]{4})\.pdf/\1/g')
-
-    echo "Assinando arquivo com GnuPG..."
-    gpg --output "Fatura - ${data} ${hora}.signed.pdf" --clearsign --not-dash-escaped "$arq"
-    [ $? -ne 0 ] && exit 4
+    echo "[$(date +'%H:%M')] Assinando arquivo com GnuPG..."
+    gpg --output "Fatura - $(date +'%Y-%m-%d %H%M').signed.pdf" --clearsign --not-dash-escaped "$arq"
+    ec=$? # "exit code" de "gpg"
+    [ $ec -ne 0 ] && exit $ec
     rm "$arq"
-    echo "Concluído!"
-    # Encerrar o agente do GnuPG para evitar assinatura sem confirmação com senha
+    echo "	Concluído!"
+    # Encerrar o agente do GnuPG para evitar assinatura posterior sem senha
     pkill gpg-agent
   fi
   sleep 1
